78d59df0e05c4e23562295e0f864fffa8f16d9f4
Defalt1 <selbyjullien117@gmail.com>
2021-05-05 02:24:52 -0400

    Proposed changes
    This commit fixes some syntax errors on homepage, adds functionality and template for easy drag and drop, adds content section, adds footer section, add header section. Adds notation on html, css and, js page. Adds placeholder image. Adds date time function. (See diff for more information)

---
+++ strapcityBC/main/index.js
@@ -0,0 +1,8 @@
+// Testing connectivity
+console.log('Hello Console');
+
+//This will keep the date in the footer of the website up-to-date yearly
+var yearInt = new Date();
+var year = yearInt.getFullYear();
+
+document.getElementById("years").innerHTML = year;
